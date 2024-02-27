e18e17763bf7da92a8c06a18766c771596fff66d
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2024-02-27 09:06:37 -0600

    chore: setup basic sensor readings

--- irrigation.ino
+++ irrigation.ino
@@ -1,8 +1,15 @@
+#include "moisture_sensor.h"
+
+Adafruit_seesaw ss;
+
 /**
  * The setup function is the main entry point, called once by the Arduino platform,
  * for initializing the software.
  */
 void setup() {
+  // Magic number provided by Adafruit sample code for the Capative moisture sensor.
+  Serial.begin(115200);
 
-}
+  MoistureSensor seensor(ss);
+} 
 
